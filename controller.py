90a535e9f412b1cff7865a974021beb8cc8bbd95
ayush-maddhesiya <ayushkumarmaddhesiya.com>
2024-10-12 04:07:11 +0530

    try-catch in controller

--- controller.py
+++ controller.py
@@ -3,11 +3,17 @@ def you_name():
   return f'Hello {name}'
 
 def you_name2():
-  incoming_data = request.json
-  name = incoming_data["name"]
-  return f'Hello {name}! How are you?'
+    incoming_data = request.json
+    if "name" not in incoming_data:
+        return jsonify({"error": "Missing field", "message": "The 'name' field is required."}), 400
+    
+    name = incoming_data["name"]
+    return f'Hello {name}! How are you?'
 
 def you_name3():
-  incoming_data = request.json
-  name = incoming_data["name"]
-  return f'Hello {name}! How are you doing today?'
+    incoming_data = request.json
+    if "name" not in incoming_data:
+        return jsonify({"error": "Missing field", "message": "The 'name' field is required."}), 400
+
+    name = incoming_data["name"]
+    return f'Hello {name}! How are you doing today?'
